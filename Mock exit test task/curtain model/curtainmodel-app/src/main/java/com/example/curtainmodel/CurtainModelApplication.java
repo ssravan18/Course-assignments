28821999770c4d747c04b423cbc8e428f3ee55d8
ssravan18 <111529264+ssravan18@users.noreply.github.com>
2024-07-17 23:27:25 +0530

    curtain task code

---
+++ Mock exit test task/curtain model/curtainmodel-app/src/main/java/com/example/curtainmodel/CurtainModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.curtainmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtainModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurtainModelApplication.class, args);
+	}
+
+}
---
+++ Mock exit test task/curtain model/curtainmodel-app/src/main/java/com/example/curtainmodel/model/Curtain.java
@@ -0,0 +1,62 @@
+package com.example.curtainmodel.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "curtain")
+public class Curtain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String brand;
+    private String material;
+    private String color;
+    private int price;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
